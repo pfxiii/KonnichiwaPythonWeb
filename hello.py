5efb8673273ba72e58b70407a4a5539d7b692cc3
pxfiii <princessfridayxiii@outlook.com>
2020-04-14 18:56:01 +0900

    セッションに記憶するよ

--- hello.py
+++ hello.py
@@ -1,9 +1,21 @@
 import flask
 
 app=flask.Flask(__name__)
+app.secret_key='pfxiii'
+app.debug = True
 
 @app.route('/')
-def hello():
-    return 'こんにちはＰｙｔｈｏｎＷｅｂ'
+def index():
+    namae=flask.session.get('namae')
+    hitokoto=flask.session.get('hitokoto')
+    return flask.render_template('index.html',namae=namae,hitokoto=hitokoto)
+
+@app.route("/toroku",methods=['POST'])
+def toroku():
+    namae=flask.request.form['namae']
+    hitokoto=flask.request.form['hitokoto']
+    flask.session['namae']=namae
+    flask.session['hitokoto']=hitokoto
+    return flask.render_template('toroku.html',namae=namae,hitokoto=hitokoto)
 
 app.run()
